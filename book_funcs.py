37405a4a3c7a9d8e75ae73e79658a16ec27ee0a9
Yotam <yotam.aflalo433@gmail.com>
2023-07-29 13:20:10 +0300

    func and svd

--- book_funcs.py
+++ book_funcs.py
@@ -127,3 +127,44 @@ def reader_encode(df,y=None):
     encoder_vars_df = pd.DataFrame(encoder_var_array, columns=encoder_name)
     df = pd.concat([df, encoder_vars_df], axis=1)
     return df
+from collections import defaultdict
+
+def precision_recall_at_k(predictions, k=10, threshold=3.5):
+    """Return precision and recall at k metrics for each user"""
+
+    # First map the predictions to each user.
+    user_est_true = defaultdict(list)
+    for uid, _, true_r, est, _ in predictions:
+        user_est_true[uid].append((est, true_r))
+
+    precisions = dict()
+    recalls = dict()
+    for uid, user_ratings in user_est_true.items():
+
+        # Sort user ratings by estimated value
+        user_ratings.sort(key=lambda x: x[0], reverse=True)
+
+        # Number of relevant items
+        n_rel = sum((true_r >= threshold) for (_, true_r) in user_ratings)
+
+        # Number of recommended items in top k
+        n_rec_k = sum((est >= threshold) for (est, _) in user_ratings[:k])
+
+        # Number of relevant and recommended items in top k
+        n_rel_and_rec_k = sum(
+            ((true_r >= threshold) and (est >= threshold))
+            for (est, true_r) in user_ratings[:k]
+        )
+
+        # Precision@K: Proportion of recommended items that are relevant
+        # When n_rec_k is 0, Precision is undefined. We here set it to 0.
+
+        #precisions[uid] = n_rel_and_rec_k / n_rec_k if n_rec_k != 0 else 1 #שיניתי ל1
+        precisions[uid] = n_rel_and_rec_k / n_rec_k if n_rec_k != 0 else None
+        # Recall@K: Proportion of relevant items that are recommended
+        # When n_rel is 0, Recall is undefined. We here set it to 0.
+        recalls[uid] = n_rel_and_rec_k / n_rel if n_rel != 0 else None
+
+        #recalls[uid] = n_rel_and_rec_k / n_rel if n_rel != 0 else 1
+
+    return precisions, recalls
